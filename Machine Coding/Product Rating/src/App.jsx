923760ef2cf5d86278dec215590387fd70be4cdd
Ritik Rana <ritikrana4@gmail.com>
2024-04-06 11:39:18 +0000

    tabs

--- Machine Coding/Product Rating/src/App.jsx
+++ Machine Coding/Product Rating/src/App.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const App = () => {
+        const [rating,setRating] = useState(0);
+        let stars = [1,2,3,4,5];
+        const handleRatingClick = (item) => {
+            setRating(item);
+        }
     return (
         <>
-            Hello
+        <div style={{display:"flex",gap:"5px"}}>
+            {stars?.map(item=>{
+                return (
+                    <div onClick={(e)=>handleRatingClick(item)} style={{height:"20px",width:"20px",background:item<=rating?"red":"white",border:"1px solid black"}}></div>
+                )
+            })}
+        </div>
         </>
     )
 }
